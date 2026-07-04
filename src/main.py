ad1de33b1a7a36386266fbf8b6cad74f1558e45d
Yash Rathi <167305367+YashRathi1035@users.noreply.github.com>
2026-07-04 11:28:04 +0530

    Adding Drawing Engine

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
 import cv2
 from camera import Camera
 from hand_tracker import HandTracker
+import numpy as np
+
+previous_x = 0
+previous_y = 0
 
 def main():
     camera = Camera()
     tracker = HandTracker()
+    canvas = None
 
     while True:
         frame = camera.read_frame()
 
         if frame is None:
             break
+
+        if canvas is None:
+            canvas = np.zeros_like(frame)
 
         frame = tracker.hand_landmarks(frame)
         landmark_list = tracker.find_position(frame)
@@ -20,12 +28,28 @@ def main():
             #cv2.putText(frame, str(landmark_list[8]), (50, 50), font, 1, (0, 0, 255), 2)
             if landmark_list:
                 fingers = tracker.finger_up(landmark_list)
+                index_x = landmark_list[8][1]
+                index_y = landmark_list[8][2]
                 
                 if fingers == [0,1,0,0,0] or fingers == [1,1,0,0,0]:
                     cv2.putText(frame,"Draw Mode",(20,50),font,1,(0,0,255),2)
-                elif fingers == [0,1,1,0,0] or fingers == [1,1,1,0,0]:
+
+                    if previous_x == 0 and previous_y == 0:
+                        previous_x = index_x
+                        previous_y = index_y
+
+                    cv2.line(canvas,(previous_x,previous_y),(index_x,index_y),(0,0,255),8)
+                    previous_x = index_x
+                    previous_y = index_y
+
+                else:
+                    previous_x = 0
+                    previous_y = 0
+
+                if fingers == [0,1,1,0,0] or fingers == [1,1,1,0,0]:
                     cv2.putText(frame,"Select Mode",(20,50),font,1,(0,0,255),2)
         
+        frame = cv2.add(frame, canvas)
         cv2.imshow("Virtual Painter Window", frame)
 
         key = cv2.waitKey(1)
