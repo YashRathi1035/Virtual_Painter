c6cf594f442ba4d39e460f4d3f2bbf5a457f7fc6
Yash Rathi <167305367+YashRathi1035@users.noreply.github.com>
2026-07-05 16:17:15 +0530

    Adding more features to the toolbar

--- src/main.py
+++ src/main.py
@@ -4,14 +4,18 @@ from hand_tracker import HandTracker
 import numpy as np
 
 
-
-
 def main():
     previous_x = 0
     previous_y = 0
 
     brush_color = (255, 0, 255)
-    brush_thickness = 8
+    small_brush = 5
+    medium_brush = 10
+    large_brush = 20
+    brush_thickness = medium_brush
+    brush_menu_open = False
+
+    eraser_thickness = 40
     toolbar_height = 100
 
     camera = Camera()
@@ -32,17 +36,7 @@ def main():
 
         h, w, _ = frame.shape
         
-        ## Toolbar
-        cv2.rectangle(frame, (0,0), (w,toolbar_height),(50,50,50), -1)
-
-        ## Red
-        cv2.rectangle(frame, (20,20), (80,80), (0,0,255), -1)
-        ## Blue
-        cv2.rectangle(frame, (100,20), (160,80), (255,0,0), -1)
-        ## Green
-        cv2.rectangle(frame, (180,20), (240,80), (0,255,0), -1)
-        ## Yello
-        cv2.rectangle(frame, (260,20), (320,80), (0,255,255), -1)
+        
 
         if len(landmark_list) != 0:
             font = cv2.FONT_HERSHEY_SIMPLEX
@@ -80,7 +74,24 @@ def main():
                         brush_color = (0,255,0)
                     elif 260 < index_x < 320:
                         brush_color = (0,255,255)
+                    elif 340 < index_x < 400:
+                        brush_color = (0,0,0)
+                    elif 420 < index_x < 480:
+                        canvas = np.zeros_like(frame)
+                    elif 500 < index_x < 560:
+                        brush_menu_open = True
 
+                if brush_menu_open:
+                    if 110 < index_y < 150:
+                        if 380 < index_x < 420:
+                            brush_thickness = small_brush
+                            brush_menu_open = False
+                        elif 440 < index_x < 480:
+                            brush_thickness = medium_brush
+                            brush_menu_open = False
+                        elif 500 < index_x < 540:
+                            brush_thickness =large_brush
+                            brush_menu_open = False
 
                 cv2.putText(frame,"Select Mode",(20,50),font,1,(0,0,255),2)
 
@@ -100,6 +111,24 @@ def main():
         cv2.rectangle(frame, (180,20), (240,80), (0,255,0), -1)
         ## Yello
         cv2.rectangle(frame, (260,20), (320,80), (0,255,255), -1)
+        ## Eraser
+        cv2.rectangle(frame, (340,20), (400,80), (255,255,255), -1)
+        cv2.putText(frame, "E", (360,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+        ## Clear
+        cv2.rectangle(frame, (420,20), (480,80), (150,150,150), -1)
+        cv2.putText(frame, "C", (440,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+        ## Brush Thickness
+        cv2.rectangle(frame, (500,20), (560,80), (150,150,150), -1)
+        cv2.putText(frame, "BT", (520,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+
+        if brush_menu_open == True:
+            cv2.rectangle(frame,(360,100),(560,160),(50,50,50),-1)
+            cv2.rectangle(frame,(380,110),(420,150),(150,150,150),-1)
+            cv2.putText(frame,"1",(390,140),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),2)
+            cv2.rectangle(frame,(440,110),(480,150),(150,150,150),-1)
+            cv2.putText(frame,"2",(450,140),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),2)
+            cv2.rectangle(frame,(500,110),(540,150),(150,150,150),-1)
+            cv2.putText(frame,"3",(510,140),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),2)
 
         cv2.circle(frame, (600, 50), 20, brush_color, -1)
 
